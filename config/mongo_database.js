7dc5f409f6720ef1953aabbe81df37f79d97870d
hima6morsi7 <ibrahim.com2011@gmail.com>
2017-07-04 07:22:38 +0200

    Update mongo_database.js

--- config/mongo_database.js
+++ config/mongo_database.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 var mongoosePaginate = require('mongoose-paginate');
 var SALT_WORK_FACTOR = 10;
-var mongodbURL = 'mongodb://localhost/productsapp';
+var mongodbURL = 'mongodb://<dbuser>:<dbpassword>@ds145952.mlab.com:45952/productapp';
 var mongodbOptions = { };
 
 mongoose.connect(mongodbURL, mongodbOptions, function (err, res) {
